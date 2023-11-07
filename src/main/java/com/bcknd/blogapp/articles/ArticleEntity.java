5fb9a2eb57780a09cb78ab5aaad43ea5920f3f84
Ashwani1330 <ashwanihabri@gmail.com>
2023-11-07 14:19:00 +0530

    Initial Commit

---
+++ src/main/java/com/bcknd/blogapp/articles/ArticleEntity.java
@@ -0,0 +1,44 @@
+package com.bcknd.blogapp.articles;
+
+import com.bcknd.blogapp.users.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.lang.Nullable;
+
+import java.util.Date;
+
+@Entity(name = "articles")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArticleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    @NonNull
+    @Column(unique = true)
+    private String slug;
+
+    @Nullable
+    private String subtitle;
+
+    @NonNull
+    private String body;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "authorId", nullable = false)
+    private UserEntity author;
+
+    // TODO: add tags
+}
---
+++ src/test/java/com/bcknd/blogapp/BlogappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcknd.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
